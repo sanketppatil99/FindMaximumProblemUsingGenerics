05a0cf475f1a26f9f97bae37f051a70a98c4bd6f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-24 22:49:37 +0530

    MaxNumber at first Position using float

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            int c = TestCase.MaxNumber(45, 56, 59);
+            float c = TestCase.MaxNumber(95.25f, 50.56f, 59.50f);
 
 
             Console.WriteLine("Maximum Number is : {0}", c);
