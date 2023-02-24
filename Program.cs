1934695e157fefa6359644e1a7d864c777e7ed69
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-24 23:30:16 +0530

    Maximum value using Generics method

--- Program.cs
+++ Program.cs
@@ -4,10 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            string c = TestCase.MaxNumber("Apple", "Banana", "Peach"); 
+            double n = TestCase<float>.MaxNumber(50.5f, 45.6f, 48.5f);
+            int a = TestCase<int>.MaxNumber(70, 60, 50);
+            string c = TestCase<string>.MaxNumber("Peach", "Apple", "Banana");
 
 
-            Console.WriteLine("Maximum Number is : {0}", c);
+
+            Console.WriteLine("Maximum int in is: {0}", a);
+            Console.WriteLine("Maximum Number in float is: {0}", n);
+            Console.WriteLine("Maximum value in string is: {0}", c);
 
         }
     }
