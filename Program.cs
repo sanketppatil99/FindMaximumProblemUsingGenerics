55d147be33a2c72812cf7b354b8319b72b658d9f
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-24 22:53:26 +0530

    MaxNumber at second Position using float

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            float c = TestCase.MaxNumber(95.25f, 50.56f, 59.50f);
+            float c = TestCase.MaxNumber(95.25f, 100.65f, 59.50f);
 
 
             Console.WriteLine("Maximum Number is : {0}", c);
